1b7d7c4c1408444a6f5d473ee48baf8acede850d
vice <vicedev@gmail.com>
2016-12-22 09:27:47 +0800

    小修改

--- app/src/main/java/com/vice/unknowweather/activity/CityManageActivity.java
+++ app/src/main/java/com/vice/unknowweather/activity/CityManageActivity.java
@@ -1,13 +1,9 @@
 package com.vice.unknowweather.activity;
 
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.support.v7.app.AppCompatActivity;
 
 import com.vice.unknowweather.R;
-import com.vice.unknowweather.global.Constants;
 
 public class CityManageActivity extends AppCompatActivity {
 
@@ -15,17 +11,6 @@ public class CityManageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_city_manage);
-
-        Button btnBack= (Button) findViewById(R.id.btn_back);
-        btnBack.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent=new Intent();
-                intent.putExtra(Constants.CITY_NAME,"上海");
-                setResult(RESULT_OK,intent);
-                finish();
-            }
-        });
 
     }
 }
